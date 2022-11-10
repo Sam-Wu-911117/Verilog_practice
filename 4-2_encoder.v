007769bf88691329affac20237df42d238e0c7f8
Sam-Wu-911117 <h4572306@gmail.com>
2022-11-10 23:06:20 +0800

    4to2mux_debug

--- 4-2_encoder.v
+++ 4-2_encoder.v
@@ -4,9 +4,9 @@ input [3:0]x;
 output [1:0]y;
 reg [1:0]y;
 if(x==4'b0001)y=2’b00;
-else if(x=4'b0010)y=2’b01;
-else if(x=4'b0100)y=2’b10;
-else if(4'b1000)y=2’b11;
+else if(x==4'b0010)y=2’b01;
+else if(x==4'b0100)y=2’b10;
+else if(x==4'b1000)y=2’b11;
 else y=2'bx;
 endmodule
 // case
